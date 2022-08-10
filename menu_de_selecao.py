15e87da2d28a53c31a58c88badea9262898d69a6
CaioLuppo <luppocode@outlook.com>
2022-08-10 00:32:23 -0300

    Adicionando opção de sair no menu

--- menu_de_selecao.py
+++ menu_de_selecao.py
@@ -11,30 +11,38 @@ def mostra_cabecalho():
       print("1 - Sequência de Fibonacci\n"
             "2 - Faturamento diário\n"
             "3 - Percentual de representação\n"
-            "4 - Inverter caracteres de uma String\n")
+            "4 - Inverter caracteres de uma String\n"
+            "sair - Sai do programa\n")
 
 def escolhe_questao():
 
       mostra_cabecalho()
 
-      options = [1, 2, 3, 4]
+      opcoes = [1, 2, 3, 4]
 
       while True:
-            option = int(input("Escolha uma opção: "))
-            if option in options:
-                  match option:
-                        case 1:
-                              print("")
-                              fibonacci.init()
-                              mostra_cabecalho()
-                        case 2:
-                              print("Op2")
-                        case 3:
-                              print("Op3")
-                        case 4:
-                              print("Op4")
-            else:
-                  print('\033[91m'+"Oops! Opção inválida, digite um número entre 1 e 4!"+'\033[0m')
+            opcao_escolhida = input("Escolha uma opção: ")
+            try:
+                  opcao = int(opcao_escolhida)
+                  if opcao in opcoes:
+                        match opcoes:
+                              case 1:
+                                    print("")
+                                    fibonacci.init()
+                                    mostra_cabecalho()
+                              case 2:
+                                    print("Op2")
+                              case 3:
+                                    print("Op3")
+                              case 4:
+                                    print("Op4")
+                  else:
+                        print('\033[91m' + "Oops! Opção inválida, digite um número entre 1 e 4!" + '\033[0m')
+            except:
+                  if opcao_escolhida == "sair":
+                        return
+                  else:
+                        print("Opção inválida")
 
 
 if __name__ == '__main__':
