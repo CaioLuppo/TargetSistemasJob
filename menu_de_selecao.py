b6712121bec6a619320fb3b978d2a5ab2ca295bd
CaioLuppo <luppocode@outlook.com>
2022-08-10 00:25:09 -0300

    Refatorando código

--- menu_de_selecao.py
+++ menu_de_selecao.py
@@ -1,33 +1,41 @@
+# imports
+from exercicios import fibonacci
 
-# Cabeçalho
-print("******************************************************\n"
-      "* PROCESSO SELETIVO TARGET SISTEMAS - By: Caio Luppo *\n"
-      "******************************************************\n")
+def mostra_cabecalho():
+      # Cabeçalho
+      print("******************************************************\n"
+            "* PROCESSO SELETIVO TARGET SISTEMAS - By: Caio Luppo *\n"
+            "******************************************************\n")
 
-# Opções
-print("1 - Sequência de Fibonacci\n"
-      "2 - Faturamento diário\n"
-      "3 - Percentual de representação\n"
-      "4 - Inverter caracteres de uma String\n")
+      # Opções
+      print("1 - Sequência de Fibonacci\n"
+            "2 - Faturamento diário\n"
+            "3 - Percentual de representação\n"
+            "4 - Inverter caracteres de uma String\n")
 
-# Código para seleção
+def escolhe_questao():
 
-options = [1, 2, 3, 4]
+      mostra_cabecalho()
 
-while True:
-      option = int(input("Escolha uma opção: "))
-      if option in options:
-            match option:
-                  case 1:
-                        print("Op1")
-                        break
-                  case 2:
-                        print("Op2")
-                        break
-                  case 3:
-                        print("Op3")
-                        break
-                  case 4:
-                        print("Op4")
-                        break
-      print('\033[91m'+"Oops! Opção inválida, digite um número entre 1 e 4!"+'\033[0m')
+      options = [1, 2, 3, 4]
+
+      while True:
+            option = int(input("Escolha uma opção: "))
+            if option in options:
+                  match option:
+                        case 1:
+                              print("")
+                              fibonacci.init()
+                              mostra_cabecalho()
+                        case 2:
+                              print("Op2")
+                        case 3:
+                              print("Op3")
+                        case 4:
+                              print("Op4")
+            else:
+                  print('\033[91m'+"Oops! Opção inválida, digite um número entre 1 e 4!"+'\033[0m')
+
+
+if __name__ == '__main__':
+    escolhe_questao()
